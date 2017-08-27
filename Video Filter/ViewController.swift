5029e09e77c654f5889fe01824d4d65ab0c32ae1
Arkadiy Glushchevsky <adya.glu@gmail.com>
2017-08-27 15:17:34 +0300

    Initial Commit

---
+++ Video Filter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Video Filter
+//
+//  Created by Arkadiy Glushchevsky on 8/27/17.
+//  Copyright © 2017 AdYa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
